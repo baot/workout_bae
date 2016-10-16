b61435a24e1bd28f8893afc4070bb1c0b6072839
Tran Bao <tran.bao51093@gmail.com>
2016-10-16 22:06:17 +0300

    add morgan for express logging

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
+/* eslint no-console: 0 */
 import express from 'express';
 import http from 'http';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import morgan from 'morgan';
+
 import userRoute from './src/api/routes/users';
 import webpackDevServer from './src/webpackDevServer';
 
@@ -10,6 +13,8 @@ const app = express();
 
 mongoose.Promise = global.Promise;  // change mongoose promise lib
 mongoose.connect('mongodb://localhost/workout');
+
+app.use(morgan('dev'));
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -20,7 +25,7 @@ webpackDevServer();
 const server = http.createServer(app);
 server.listen(3000, 'localhost', (err) => {
   if (err) {
-    return console.log(err);
+    return console.error(err);
   }
 
   return console.log('API server listening at http://localhost:3000');
